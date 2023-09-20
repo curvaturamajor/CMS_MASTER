fa243a680ee208049e86e867f08792b103710126
CurvaturaMajor <62203446+curvaturamajor@users.noreply.github.com>
2023-09-20 23:11:04 +0300

    Add files via upload

---
+++ CMSUI/LoginWindows/DatabaseSettingWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CMSLibrary;
+using CMSUI.Requesters;
+using System.IO;
+using System.Windows;
+
+namespace CMSUI.LoginWindows
+{
+    /// <summary>
+    /// Interaction logic for DatabaseSettingWindow.xaml
+    /// </summary>
+    public partial class DatabaseSettingWindow
+    {
+        IDatabaseSettingRequester CallinWindow;
+        public DatabaseSettingWindow(IDatabaseSettingRequester caller)
+        {
+            InitializeComponent();
+            CallinWindow = caller;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            GlobalConfig.Ip = ipText.Text;
+            GlobalConfig.Port = portText.Text;
+            GlobalConfig.Username = usernameText.Text;
+            GlobalConfig.Password = passwordText.Password;
+            File.WriteAllText($"{System.AppDomain.CurrentDomain.BaseDirectory}info.txt", $"{GlobalConfig.Ip};{GlobalConfig.Port};{GlobalConfig.Username};{GlobalConfig.Password};");
+            this.Close();
+            CallinWindow.DatabaseSettingSaved();
+        }
+    }
+}
---
+++ CMSUI/LoginWindows/DevelopersInfoWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace CMSUI.LoginWindows
+{
+    /// <summary>
+    /// Interaction logic for DevelopersInfoWindow.xaml
+    /// </summary>
+    public partial class DevelopersInfoWindow
+    {
+        public DevelopersInfoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
