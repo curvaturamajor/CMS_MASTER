80982855e22d467d64b4d785885a2e3db57a6e72
CurvaturaMajor <62203446+curvaturamajor@users.noreply.github.com>
2023-10-16 20:23:57 +0300

    Add files via upload

---
+++ CMSUI/Requesters/ICouresRequester.cs
@@ -0,0 +1,10 @@
+﻿using CMSLibrary.Models;
+
+namespace CMSUI.Requesters
+{
+    public interface ICouresRequester
+    {
+        void CourseComplete(CourseModel model);
+        void CourseUpdateComplete(CourseModel model);
+    }
+}
