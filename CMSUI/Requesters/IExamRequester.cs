d2748bada2ca08aa3eea45f85d8b6512e92d27ef
CurvaturaMajor <62203446+curvaturamajor@users.noreply.github.com>
2023-10-16 20:25:29 +0300

    Add files via upload

---
+++ CMSUI/Requesters/IExamRequester.cs
@@ -0,0 +1,12 @@
+﻿using CMSLibrary.Models;
+
+
+namespace CMSUI.Requesters
+{
+    public interface IExamRequester
+    {
+        void ExamComplete(ExamModel model);
+        UserModel GetUserInfo();
+        AssignmentModel GetAssignment();
+    }
+}
